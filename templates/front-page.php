446340af2a88d45959aca57e3ecf59f5a0fe270c
jeremyfelt <jeremy.felt@gmail.com>
2014-09-30 14:55:59 -0500

    Properly show manual entry items on front page
    This will change as we allow for byline and source to be added

--- templates/front-page.php
+++ templates/front-page.php
@@ -41,7 +41,14 @@
 					$link_url = get_post_meta( get_the_ID(), '_feed_item_link_url', true );
 					$link_author = ucwords( strtolower( get_post_meta( get_the_ID(), '_feed_item_author', true ) ) );
 					$source_id = get_post_meta( get_the_ID(), '_feed_item_source', true );
-					$source = get_post( absint( $source_id ) );
+					if ( empty( $source_id ) ) {
+						$source_title = 'Manual Entry';
+						$link_url = get_the_permalink();
+						$link_author = 'By ' . get_the_author();
+					} else {
+						$source = get_post( absint( $source_id ) );
+						$source_title = $source->post_title;
+					}
 					?>
 
 					<article class="recent-article">
@@ -49,7 +56,7 @@
 						<h3 class="recent-article-title"><a href="<?php echo esc_url( $link_url ); ?>"><?php the_title(); ?></a></h3>
 						<span class="recent-article-date"><?php echo get_the_date(); ?></span>
 						<span class="recent-article-author"><?php echo esc_html( $link_author ); ?></span>
-						<span class="recent-article-source"><?php echo $source->post_title; ?></span>
+						<span class="recent-article-source"><?php echo $source_title; ?></span>
 						<?php the_excerpt(); ?>
 					</article>
 				<?php
